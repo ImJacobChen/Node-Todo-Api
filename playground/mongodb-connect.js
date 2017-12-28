3150ef33566aada718372fc425625b8361ff4191
JacobChen1994 <jacobsenpai@outlook.com>
2017-12-28 19:18:44 +0000

    Add mongodb query examples

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -17,16 +17,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(res.ops, undefined, 2));
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Jacob',
-        age: 24,
-        location: 'London'
-    }, (err, res) => {
-        if (err) {
-            return console.log('Failed to insert Todo', err);
-        }
-        console.log(JSON.stringify(res.ops, undefined, 2));
+    // db.collection('Users').insertOne({
+    //     name: 'Jacob',
+    //     age: 24,
+    //     location: 'London'
+    // }, (err, res) => {
+    //     if (err) {
+    //         return console.log('Failed to insert Todo', err);
+    //     }
+    //     console.log(JSON.stringify(res.ops, undefined, 2));
+    // });
+
+    db.collections('Todos').find().toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log(err);
     });
 
-    db.close();
+    db.collections('Users').find({name: 'Jacob'}).toArray().then((users) => {
+        console.log(JSON.stringify(users, undefined, 2));
+    }, (err) => {
+        console.log(err);
+    });
 });
