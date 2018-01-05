c907c4828ad767e2d724898a47ba9720d7f6440c
Jacob Chen <jacobchen17@googlemail.com>
2018-01-05 18:15:09 +0000

    - Connect to the Atlas mongodb database. - Create a User mongoose model and experiment with validation

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://jacobchen17:securepassword@cluster0-shard-00-00-qy9zz.mongodb.net:27017,cluster0-shard-00-01-qy9zz.mongodb.net:27017,cluster0-shard-00-02-qy9zz.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
 var Todo = mongoose.model('Todo', {
     text: {
@@ -15,12 +15,21 @@ var Todo = mongoose.model('Todo', {
     }
 });
 
-var newTodo = new Todo({
-    text: 'Cook dinner'
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+    }
 });
 
-newTodo.save().then((res) => {
-    console.log(res);
-}, (err) => {
+var user = new User({
+    email: 'Jacob@example.com'
+});
+
+user.save().then((res) => {
+    console.log('User saved');
+}).catch((err) => {
     console.log(err);
-})
+});
